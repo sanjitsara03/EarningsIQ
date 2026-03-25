1ce569dfc377d204f07c2d2ee34e48435bbd69b7
sanjitsara03 <sanjitsara03@gmail.com>
2026-03-25 17:58:49 -0400

    Minor fixes

--- api/main.py
+++ api/main.py
@@ -20,7 +20,6 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://*.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
